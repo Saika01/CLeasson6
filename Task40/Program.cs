0b2bd5838bb14f01d93f34e536583a59805746f9
Aoi <naztya2001@bk.ru>
2023-03-12 10:43:24 +0300

    task40

---
+++ Task40/Program.cs
@@ -0,0 +1,16 @@
+﻿// Каждая сторона треугольника меньше 2х других
+
+Console.WriteLine("Input 1st number");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input 2nd number");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input 3rd number");
+int c = Convert.ToInt32(Console.ReadLine());
+
+bool IfTriangleExists(int one, int two, int three)
+{
+    return one+two>three && two+three>one && three+one>two;
+}
+
+if(IfTriangleExists(a,b,c)) Console.WriteLine("Yes");
+else Console.WriteLine("No");
