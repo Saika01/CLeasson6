3dd442c05ec55d7de5a19f29d4f9be392f78b696
Aoi <naztya2001@bk.ru>
2023-03-12 11:37:18 +0300

    task44

---
+++ Task44/Program.cs
@@ -0,0 +1,39 @@
+﻿int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void ArrayPrinted(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write(arr[i] + ",");
+        else Console.Write(arr[i]);
+    }
+    Console.WriteLine("]");
+}
+
+int[] Fibonacci(int num)
+{
+    int[] array = new int[num];
+    array[1] = 1;
+    for (int i = 2; i < num; i++)
+    {
+        array[i] = array[i-1]+ array[i-2];
+    }
+    return array;
+}
+
+Console.WriteLine("Input number");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] arr = Fibonacci(number);
+ArrayPrinted(arr);
+
+
